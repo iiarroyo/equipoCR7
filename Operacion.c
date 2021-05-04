649d3be1626dfa37fb59f58d30b612f2503c6418
mikelv21 <a01706424@itesm.mx>
2021-05-04 12:58:36 -0500

    Código depurado

--- Operacion.c
+++ Operacion.c
@@ -1,14 +1,6 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
-
-int suma(int a, int b){
- return a+b;
-}
-int resta(int a, int b){
- return a-b;
-}
 
 int main(){
 int num1 = 12;
@@ -18,8 +10,8 @@ int op1 = suma(num1,num2);
 printf("%i",op1);
 int op2 = resta(num1,num2);
 printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
+
 
 return 0;
+}
 
