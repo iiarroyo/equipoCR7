78f43d4a7df1bea5c6a4f5b16a3a709eabfb9291
israel-iap <israel.iap@outlook.com>
2021-05-06 17:35:46 -0500

    añade funcionalidad de equalizacion

--- hist.py
+++ hist.py
@@ -3,9 +3,16 @@ import cv2
 import matplotlib.pyplot as plt
 
 
-image = cv2.imread("noki.jpg",cv2.IMREAD_GRAYSCALE)
+image = cv2.imread("persona.jpg",cv2.IMREAD_GRAYSCALE)
 h = [0]*256
 for value in range(256):
     h[value] = (image==value).sum()
+
 plt.bar(range(256),h)
+plt.show()
+
+equ = cv2.equalizeHist(image)
+res = np.hstack((image,equ)) #stacking images side-by-side
+plt.imshow(res,cmap='gray')
+plt.title("imagen eq")
 plt.show()
