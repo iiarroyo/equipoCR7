636657b5bb69064ffdff5942badd5753df47f3c8
Kevin Joan <A01706328@itesm.mx>
2021-05-06 21:55:49 -0500

    Agregados comentarios

--- convolucion_proyecto.py
+++ convolucion_proyecto.py
@@ -9,43 +9,43 @@ import matplotlib.pyplot as plt
 def suma_matrices(matriz,kernel):
     """multiplicar la matriz cortada y la de kernel para devolver la suma"""
 
-    m_row, m_col = matriz.shape                   
-    k_row, k_col = kernel.shape                       
-    resultado = 0                               
+    m_row, m_col = matriz.shape                       #asignar el tamaño de la matriz (en filas y columnas)
+    k_row, k_col = kernel.shape                       #asignar el tamaño del kernel (en filas y columnas)
+    resultado = 0                                   #instanciar la resultante en ceros
 
-    for row in range (m_row):                               
-        for col in range (m_col):                       
-            resultado+= matriz[row,col] * kernel[row,col]      
-    return resultado                                       
+    for row in range (m_row):                                #recorrer filas de la matriz
+        for col in range (m_col):                            #recorrer columnas de la matriz
+            resultado+= matriz[row,col] * kernel[row,col]       #acumular el resultado de cada una de las multiplicaciones entre las filas y columnas de la matriz y el kernel
+    return resultado                                         #devolver resultante
  
 def convolucion(imagen, kernel):
     """Aplicar la convolución según la imagen, al ser de 3 canales o dimensiones, se cambiaran a dos canales"""
     
     if len(imagen.shape) == 3:
-        print("Found 3 Channels : {}".format(imagen.shape)) 
+        print("Found 3 Channels : {}".format(imagen.shape)) #Se econtraron 3 canales
         imagen = cv2.cvtColor(imagen, cv2.COLOR_BGR2GRAY)
-        print("Converted to Gray Channel. Size : {}".format(imagen.shape)) 
+        print("Converted to Gray Channel. Size : {}".format(imagen.shape)) #Convertido a dos dimensiones/escala de grises
     else:
-        print("Imagen Shape : {}".format(imagen.shape)) 
+        print("Imagen Shape : {}".format(imagen.shape)) #Tamaño de la imagen
  
-    imagen_row, imagen_col = imagen.shape 
-    kernel_row, kernel_col = kernel.shape 
+    imagen_row, imagen_col = imagen.shape #asignar el alto y ancho de la imagen
+    kernel_row, kernel_col = kernel.shape #asignar el alto y ancho del filtro
     
-    output_x = (imagen_col - (kernel_col / 2) * 2) + 1 
-    output_y = (imagen_row - (kernel_row / 2) * 2) + 1 
+    output_x = (imagen_col - (kernel_col / 2) * 2) + 1 #asigna el ancho del output
+    output_y = (imagen_row - (kernel_row / 2) * 2) + 1 #asigna el alto del output
     
-    output_x_int = int (output_x) 
-    output_y_int = int (output_y) 
+    output_x_int = int (output_x) #cambiar los valores a enteros
+    output_y_int = int (output_y) #cambiar los valores a enteros
     
-    output = np.zeros([output_y_int, output_x_int])
+    output = np.zeros([output_y_int, output_x_int]) #matriz donde se guardan las resultantes de las coordenadas
  
-    for row in range(output_y_int): 
-        for col in range(output_x_int):
+    for row in range(output_y_int): #recorrer las filas de la imagen
+        for col in range(output_x_int): #recorrer las columnas de la imagen
                 output[row,col] = suma_matrices( 
                                     imagen[row:row + kernel_row, 
-                                    col:col + kernel_col],kernel) 
+                                    col:col + kernel_col],kernel) #relizar la suma entre las filas de la matriz con la del kernel y lo mismo con las columnas
  
-    print("Output Imagen size : {}".format(output.shape)) 
+    print("Output Imagen size : {}".format(output.shape)) #Tamaño de la imagen final
  
     return output
     
